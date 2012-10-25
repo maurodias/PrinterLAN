d0676569fafe5448cdd1013aa827e4c81f839a1a
maurodias <luimauropcdias@yahoo.com.br>
2012-10-24 23:27:38 -0200

    Fix bugs - com ajuda de Fábio, que na verdade não ajudou nada

--- servidor.py
+++ servidor.py
@@ -4,6 +4,7 @@ from maquina import Maquina
 from computador import Computador
 
 class Servidor(Computador):
+ 
     def __init__(self, codigo_patrimonio, descricao ,hd,memoria,tamanho_max_buffer,quantidade_max_buffer):
         super(Servidor,self).__init__(codigo_patrimonio, descricao ,hd,memoria)
         self._validar_valor(quantidade_max_buffer)
@@ -11,6 +12,7 @@ class Servidor(Computador):
         self._validar_valor(tamanho_max_buffer)
         self._tamanho_max_buffer = tamanho_max_buffer
         self._impressoras=[]
+        #cada servidor pode ter até 3 impressoras
         
     def alterar_quantidade_max_buffer(self,quantidade_max_buffer):
         self._validar_valor(quantidade_max_buffer)
@@ -41,7 +43,8 @@ class Servidor(Computador):
     
     def verificar_disponibilidade(self):
         if len(self._impressoras) >= 3 :
-            raise ValueError 
+            raise ValueError
+        return True 
             
     impressoras = property(obter_impressoras)
     tamanho_max_buffer = property(obter_tamanho_max_buffer, alterar_tamanho_max_buffer)
